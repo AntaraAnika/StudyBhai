04a330a36f077191ef77118ee453a4523d2265c6
AntaraAnika <anika@insurecow.com>
2022-04-30 20:52:50 +0600

    yy

---
+++ SignIn_SignUp/script.js
@@ -0,0 +1,11 @@
+
+
+function register(event){
+    event.preventDefault();
+    document.querySelector('#sign-in-section').classList.add('animation');
+}
+
+function handleGetIn(event){
+    event.preventDefault();
+    document.querySelector('#sign-in-section').classList.remove('animation');
+}
